ff61cf09d157b31f7c4fc449af958f907b3db8d0
marcobudelli5 <marcobudelli5@LAPTOP-69PHNFAR.localdomain>
2025-10-27 23:02:07 +0100

    Updated a new file

---
+++ e02/sommanumerinaturali.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+
+somma = 0
+for i in range(1, 101):
+   somma = somma+i
+print('La somma dei primi 100 numeri naturali è ', somma)
