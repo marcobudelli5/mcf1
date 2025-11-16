76ce441693fc68b4a120e4f5e82b3207782de978
marcobudelli5 <marcobudelli5@LAPTOP-69PHNFAR.localdomain>
2025-11-16 22:34:04 +0100

    modifica effettuata

--- esercitazione_funzioni/somme.py
+++ esercitazione_funzioni/somme.py
@@ -3,9 +3,9 @@ def somma_n_numeri(n):
      for i in range(n+1):
           somma = somma+i
      return somma
-def somma_n_numeri2(n):
-     somma1 = (n*(n+1))/2
-     somma2 = int(somma1)
-     return somma2
+#def somma_n_numeri2(n):
+     #somma1 = (n*(n+1))/2
+    # somma2 = int(somma1)
+     #return somma2
 print(somma_n_numeri(10))
-print(somma_n_numeri2(10))
+#print(somma_n_numeri2(10))
