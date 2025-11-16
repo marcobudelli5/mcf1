b0b1c3d813ffceb94bb3a37145bb8858626128e0
marcobudelli5 <marcobudelli5@LAPTOP-69PHNFAR.localdomain>
2025-11-16 23:40:45 +0100

    modifica fatta

--- esercitazione_funzioni/script_somme.py
+++ esercitazione_funzioni/script_somme.py
@@ -1,3 +1,6 @@
 import somme as sm
-print(sm.somma_n_numeri(20))
-print(sm.somma_radici(5))
+print('Ecco la somma dei primi 10 numeri naturali: ', sm.somma_n_numeri(10))
+print('Ecco la somma delle radici dei primi 5 numeri naturali: ', sm.somma_radici(5))
+s, p= sm.somma_prodotto_n_numeri(10)
+print('Ecco rispettivamente somma e prodotto dei primi 10 numeri naturali: ', s, ',', p)
+print('Ecco la sommatoria con l indice i che varia da 0 a 5 di i elevato alla seconda: ', sm.sommatoria_potenze_alfa(5, 2))
