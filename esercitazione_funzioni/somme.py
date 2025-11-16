85a49b4a85c956dacdb0bcbca4a73eb331056394
marcobudelli5 <marcobudelli5@LAPTOP-69PHNFAR.localdomain>
2025-11-16 23:27:19 +0100

    modifica effettuata

--- esercitazione_funzioni/somme.py
+++ esercitazione_funzioni/somme.py
@@ -1,15 +1,30 @@
 import math as mt
-def somma_n_numeri(n):
+def somma_prodotto_n_numeri(n):
      somma=0
-     for i in range(n+1):
+     for i in range(1, n+1):
           somma = somma+i
      return somma
+
+def somma_prodotto_n_numeri(n):
+     somma=0
+     prodotto=1
+     for i in range(1, n+1):
+          somma = somma+i
+          prodotto=prodotto*i
+     return somma, prodotto
+
 def somma_radici(n):
      somma_r=0
      for i in range(n+1):
           radice = mt.sqrt(i)
           somma_r=somma_r+radice
      return somma_r
+
+def sommatoria_potenze_alfa(n,a=1):
+     sommatoria=0
+     for i in range(0, n+1):
+          sommatoria=sommatoria+mt.pow(i, a)
+     return sommatoria
      
 #def somma_n_numeri2(n):
      #somma1 = (n*(n+1))/2
